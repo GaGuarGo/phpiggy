e62214b08c6b3b2166444cff0d704557a5e9713c
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-02-20 19:18:26 -0300

    Custom Validation Exception

--- src/Framework/Validator.php
+++ src/Framework/Validator.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Framework;
 
 use Framework\Contracts\RuleInterface;
+use Framework\Exceptions\ValidationExcetpion;
 
 class Validator
 {
@@ -37,7 +38,7 @@ class Validator
         }
 
         if (count($errors)) {
-            dd($errors);
+            throw new ValidationExcetpion();
         }
     }
 }
