01da12910298f0ab7be85ff31cbc74f16d390377
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-02-20 18:22:49 -0300

    Validation Rule Contract

--- src/Framework/Validator.php
+++ src/Framework/Validator.php
@@ -4,8 +4,18 @@ declare(strict_types=1);
 
 namespace Framework;
 
+use Framework\Contracts\RuleInterface;
+
 class Validator
 {
+
+    private $rules = [];
+
+    public function add(string $alias, RuleInterface $rule)
+    {
+
+        $this->rules[$alias] = $rule;
+    }
 
     public function validate(array $formData)
     {
