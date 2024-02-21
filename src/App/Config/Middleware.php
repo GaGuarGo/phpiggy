4258032df22bf92bbd0de068146f20ce327b528c
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-02-21 16:09:14 -0300

    Injectors Errors into a Template

--- src/App/Config/Middleware.php
+++ src/App/Config/Middleware.php
@@ -5,7 +5,12 @@ declare(strict_types=1);
 namespace App\Config;
 
 use Framework\App;
-use App\Middleware\{TemplateDataMiddleware, ValidationExceptionMiddleware, SessionMiddleware};
+use App\Middleware\{
+    TemplateDataMiddleware,
+    ValidationExceptionMiddleware,
+    SessionMiddleware,
+    FlashMiddleware
+};
 
 
 
@@ -13,5 +18,6 @@ function registerMiddleware(App $app)
 {
     $app->addMiddleware(TemplateDataMiddleware::class);
     $app->addMiddleware(ValidationExceptionMiddleware::class);
+    $app->addMiddleware(FlashMiddleware::class);
     $app->addMiddleware(SessionMiddleware::class);
 }
