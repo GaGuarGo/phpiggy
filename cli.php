849fd3b9a1cca7f1ec841c75971c0890c293db5c
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-02-23 12:09:54 -0300

    Creating a DSN String

--- cli.php
+++ cli.php
@@ -1,3 +1,18 @@
 <?php
 
-echo  "Hellow World!";
+$driver = 'mysql';
+$configuration = http_build_query(
+
+    data: [
+        'host' => 'localhost',
+        'port' => 3306,
+        'dbname' => 'phpiggy',
+
+    ],
+    arg_separator: ';',
+);
+
+$dsn = "{$driver}:{$configuration}";
+
+
+echo $dsn;
