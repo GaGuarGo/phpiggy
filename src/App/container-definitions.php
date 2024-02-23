25cf45c89de5770b1066228e8ef1bec1af7f742e
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-02-23 14:50:07 -0300

    Creating Environment Variables

--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -10,13 +10,13 @@ return [
     TemplateEngine::class => fn () => new TemplateEngine(Paths::VIEW),
     ValidatorService::class => fn () => new ValidatorService(),
     Database::class => fn () => new Database(
-        driver: 'mysql',
+        driver: $_ENV['DB_DRIVER'],
         config: [
-            'host' => 'localhost',
-            'port' => 3306,
-            'dbname' => 'phpiggy'
+            'host' => $_ENV['DB_HOTS'],
+            'port' => $_ENV['DB_PORT'],
+            'dbname' => $_ENV['DB_NAME']
         ],
-        username: 'root',
-        password: '',
+        username: $_ENV['DB_USER'],
+        password: $_ENV['DB_PASS'],
     ),
 ];
