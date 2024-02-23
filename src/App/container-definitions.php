9b1180a80d0f4e1807194bdefb3ce194adb52ed4
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-02-23 14:36:08 -0300

    Database Container Definition

--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -2,11 +2,21 @@
 
 declare(strict_types=1);
 
-use Framework\TemplateEngine;
+use Framework\{TemplateEngine, Database};
 use App\Config\Paths;
 use App\Services\ValidatorService;
 
 return [
     TemplateEngine::class => fn () => new TemplateEngine(Paths::VIEW),
     ValidatorService::class => fn () => new ValidatorService(),
+    Database::class => fn () => new Database(
+        driver: 'mysql',
+        config: [
+            'host' => 'localhost',
+            'port' => 3306,
+            'dbname' => 'phpiggy'
+        ],
+        username: 'root',
+        password: '',
+    ),
 ];
