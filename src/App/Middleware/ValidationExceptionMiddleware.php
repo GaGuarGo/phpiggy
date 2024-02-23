b266ea42e31d62e7e60ddd96bbe17f20f901c9b5
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-02-23 09:40:00 -0300

    Filtering Sensitive Data

--- src/App/Middleware/ValidationExceptionMiddleware.php
+++ src/App/Middleware/ValidationExceptionMiddleware.php
@@ -17,10 +17,18 @@ class ValidationExceptionMiddleware implements MiddlewareInterface
             $next();
         } catch (ValidationException $e) {
 
-            $_SESSION['errors'] = $e->errors;
-            $_SESSION['oldFormData'] = $_POST .
+            $oldFormData = $_POST;
 
-                $referer = $_SERVER['HTTP_REFERER'];
+            $excludedFields = ['password', 'confirmPassword'];
+            $formattedFormData = array_diff_key(
+                $oldFormData,
+                array_flip($excludedFields)
+            );
+
+            $_SESSION['errors'] = $e->errors;
+            $_SESSION['oldFormData'] = $formattedFormData;
+
+            $referer = $_SERVER['HTTP_REFERER'];
             redirectTo($referer);
         }
     }
